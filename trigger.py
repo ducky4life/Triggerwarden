ea4dda9c2f6cce3339c7c2566612136e375d975b
Ducky <124077142+ducky4life@users.noreply.github.com>
2025-03-07 09:14:01 +0800

    region triggers

--- trigger.py
+++ trigger.py
@@ -7,7 +7,6 @@ import keep_alive
 from dotenv import load_dotenv
 import sans
 
-
 intents = discord.Intents.all()
 intents.members = True
 
@@ -25,19 +24,27 @@ async def on_ready():
 
 
 
-async def main(nations, channel_id):
+async def nation(nations, channel_id):
     async with sans.AsyncClient() as asyncclient:
         async for event in sans.serversent_events(asyncclient, "change").view(nations=nations):
             channel = await client.fetch_channel(channel_id)
-            await channel.send(event["str"])
+            if "influence in" in event["str"]:
+                await channel.send(event["str"])
 
+
+async def region(regions, channel_id):
+    async with sans.AsyncClient() as asyncclient:
+        async for event in sans.serversent_events(asyncclient, "admin").view(regions=regions):
+            channel = await client.fetch_channel(channel_id)
+            if "updated." in event["str"]:
+                await channel.send(event["str"])
 
 
 @client.hybrid_command(description="connect to sse with the changes bucket")
 @app_commands.describe(nations="list of nations to track, separated by commas. do not put spaces after commas. e.g. 'nation1,nation2,nation3'", channel="what channel to send sse activities to")
 async def nationtrigger(ctx, nations:str=None, channel:discord.TextChannel=None):
     nation_list = nations.split(",")
-    task = asyncio.create_task(main(nation_list, channel.id))
+    task = asyncio.create_task(nation(nation_list, channel.id))
 
     class Buttons(discord.ui.View):
         @discord.ui.button(label='Stop SSE', style=discord.ButtonStyle.red)
@@ -47,6 +54,20 @@ async def nationtrigger(ctx, nations:str=None, channel:discord.TextChannel=None)
 
     await ctx.send("Connected, click button to stop SSE",view=Buttons())
 
+
+@client.hybrid_command(description="connect to sse with the admin bucket")
+@app_commands.describe(regions="list of nations to track, separated by commas. do not put spaces after commas. e.g. 'nation1,nation2,nation3'", channel="what channel to send sse activities to")
+async def regiontrigger(ctx, regions:str=None, channel:discord.TextChannel=None):
+    region_list = regions.split(",")
+    task = asyncio.create_task(region(region_list, channel.id))
+
+    class Buttons(discord.ui.View):
+        @discord.ui.button(label='Stop SSE', style=discord.ButtonStyle.red)
+        async def on_button_click(self, interaction: discord.Interaction, button: discord.ui.Button) -> None:
+            task.cancel()
+            await interaction.response.edit_message(content='SSE has been stopped', view=None)
+
+    await ctx.send("Connected, click button to stop SSE",view=Buttons())
 
 
 keep_alive.keep_alive()
